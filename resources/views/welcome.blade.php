2733373e745381ae714026f26a474b742b47504c
Kristoffer Lorenzen <kristoffercl@hotmail.com>
2025-06-09 12:11:07 +0200

    Update welcome page with header and hero section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,40 @@
     <title>Document</title>
 </head>
 <body>
-    hello
+    <header class="header">
+        <div class="topbar">
+            <div class="brand">Chronen</div>
+            <nav class="navigation">
+                <ul class="navigation-list">
+                    <li class="navigation-item">
+                        <a href="#" class="navigation-link">Om mig</a>
+                    </li>
+                    <li class="navigation-item">
+                        <a href="#" class="navigation-link">Portfolio</a>
+                    </li>
+                    <li class="navigation-item">
+                        <a href="#" class="navigation-link">Kontakt</a>
+                    </li>
+                    <li class="navigation-item navigation-item--cv">
+                        <a href="#" class="navigation-link">Hent CV</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="main">
+
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Kristoffer Chrone Lorenzen</h1>
+        </div>
+        <div class="hero-image">
+            <img src="{{asset('images/me2.jpg')}}" alt="">
+        </div>
+    </section>
+        
+    </main>
+
+
 </body>
 </html>
